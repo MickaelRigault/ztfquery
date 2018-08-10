140ada4b40f9b2f84fb0fc15dd55f8f48d11b277
MickaelRigault <mick.rigault@gmail.com>
2018-08-10 12:29:57 +0200

    first installation bug solved

--- ztfquery/__init__.py
+++ ztfquery/__init__.py
@@ -1,6 +1,7 @@
 
-__version__ = "0.6.0"
+__version__ = "0.6.1"
 
 # - First time you call the script, it'll ask for your login info
-from .io import has_encryption, encrypt
-if not has_encryption(): encrypt()
+from .io import has_encryption, set_account
+if not has_encryption():
+    set_account("irsa")
