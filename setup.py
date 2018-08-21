414e740cea420970c83c829eccc052c146941699
MickaelRigault <mick.rigault@gmail.com>
2018-08-21 16:04:15 +0200

    now 2.7 compatible

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ MAINTAINER = 'Mickael Rigault'
 MAINTAINER_EMAIL = 'm.rigault@ipnl.in2p3.fr'
 URL = 'https://github.com/MickaelRigault/ztfquery'
 LICENSE = 'BSD (3-clause)'
-DOWNLOAD_URL = 'https://github.com/MickaelRigault/ztfquery/tarball/1.0'
-VERSION = '1.0.1'
+DOWNLOAD_URL = 'https://github.com/MickaelRigault/ztfquery/tarball/1.1'
+VERSION = '1.1.0'
 
 try:
     from setuptools import setup, find_packages
