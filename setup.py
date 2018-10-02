d400560e3aa0f3051ec33eb962d29ef24e82c93d
MickaelRigault <mick.rigault@gmail.com>
2018-10-02 17:13:05 +0200

    New field query test

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ MAINTAINER_EMAIL = 'm.rigault@ipnl.in2p3.fr'
 URL = 'https://github.com/MickaelRigault/ztfquery'
 LICENSE = 'BSD (3-clause)'
 DOWNLOAD_URL = 'https://github.com/MickaelRigault/ztfquery/tarball/1.2'
-VERSION = '1.2.3'
+VERSION = '1.2.4'
 
 try:
     from setuptools import setup, find_packages
--- ztfquery/__init__.py
+++ ztfquery/__init__.py
@@ -1,4 +1,4 @@
 
-__version__ = "1.2.3"
+__version__ = "1.2.4"
 
 # - First time you call the script, it'll ask for your login info
