ed3a47700cba6a7e1d77fa06cd6cf9b88a95549c
MickaelRigault <mick.rigault@gmail.com>
2021-02-18 15:35:33 +0100

    cannot guess fracday from obsjd

--- ztfquery/buildurl.py
+++ ztfquery/buildurl.py
@@ -252,27 +252,15 @@ def parse_filename(filename):
             "filtercode":filtercode, "ccdid":ccd_, "imgtypecode":imgtypecode,
             "qid":qid_, "suffix":suffix_}
 
-def obsjd_to_filefrac(obsjd):
-    """ """
-    from astropy import time
-    import pandas
-    if type(obsjd) != pandas.Series:
-        obsjd = pandas.Series(obsjd)
-        
-    fracday = ( (obsjd - obsjd.astype("int")-0.5)*1000000 ).astype("int")
-    day     = pandas.Series(time.Time(obsjd.astype(float), format="jd").iso, index=obsjd.index
-                       ).str.split(" ", expand=True)[0].str.replace("-","")
-    return day.astype(str)+fracday.astype(str)
-
 def build_filename_from_dataframe(dataframe, suffix="sciimg.fits"):
     """ The dataframe must contains:
-    obsjd, fieldid, ccdid, qid and filterid
+    filefracday, fieldid, ccdid, qid and filterid
 
     Returns
     ------
     pandas.Series (name of the files (not fullpath))
     """
-    filefrac = obsjd_to_filefrac(dataframe["obsjd"])
+    filefrac    = dataframe["filefracday"].astype("int").astype("str")
     paddedfield = dataframe["fieldid"].astype("int").astype("str").str.pad(6, fillchar="0")
     paddedccd = "c"+dataframe["ccdid"].astype("int").astype("str").str.pad(2, fillchar="0")
     paddedqid = "q"+dataframe["qid"].astype("int").astype("str")
