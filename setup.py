a48f51a7126fe5d7101d4b419381946349e6d0c7
MickaelRigault <mick.rigault@gmail.com>
2021-09-28 10:22:52 +0200

    moving to 0.16.0

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ MAINTAINER = 'Mickael Rigault'
 MAINTAINER_EMAIL = 'm.rigault@ipnl.in2p3.fr'
 URL = 'https://github.com/MickaelRigault/ztfquery'
 LICENSE = 'BSD (3-clause)'
-DOWNLOAD_URL = 'https://github.com/MickaelRigault/ztfquery/tarball/1.15'
-VERSION = '1.15.9'
+DOWNLOAD_URL = 'https://github.com/MickaelRigault/ztfquery/tarball/1.16'
+VERSION = '1.16.0'
 
 try:
     from setuptools import setup, find_packages
--- ztfquery/__init__.py
+++ ztfquery/__init__.py
@@ -1,4 +1,4 @@
 
-__version__ = "1.15.9"
+__version__ = "1.16.0"
 
 from .io import get_file
