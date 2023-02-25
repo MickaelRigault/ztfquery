ad30135b8e58446855cbeb6bca798b08914a22a0
MickaelRigault <mick.rigault@gmail.com>
2023-02-25 15:28:12 +0100

    merging bug fixed

--- ztfquery/__init__.py
+++ ztfquery/__init__.py
@@ -1,6 +1,6 @@
 __version__ = "1.24.2"
 
 from .io import get_file
-from .query import get_metadata
+from .query import get_metadata, ZTFQuery
 
 # from .skyvision import get_summary_logs
--- ztfquery/buildurl.py
+++ ztfquery/buildurl.py
@@ -170,7 +170,10 @@ def parse_rawfilename(filename):
 def parse_scifilename(filename):
     """ """
     from .fields import ccdid_qid_to_rcid
-
+    
+    _, filefracday, paddedfield, filtercode, ccd_, suffix_ = os.path.basename( filename ).split("_")
+    year, month, day, fracday = filefrac_to_year_monthday_fracday(filefracday)
+    
     ccdid = int(ccd_.replace("c", ""))
     qid = int(qid_.replace("q", ""))
     return {
