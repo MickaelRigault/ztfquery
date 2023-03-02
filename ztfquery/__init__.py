38a492700ad2dba18220ca5dae3a9e2333bfa15e
simeonreusch <simeon.reusch@desy.de>
2023-03-02 12:02:52 +0100

    bump version, add pypi workflow to CI

--- ztfquery/__init__.py
+++ ztfquery/__init__.py
@@ -1,6 +1,6 @@
-__version__ = "1.24.6"
+__version__ = "1.24.7"
 
 from .io import get_file
-from .query import get_metadata, ZTFQuery
+from .query import ZTFQuery, get_metadata
 
 # from .skyvision import get_summary_logs
